ae184ca59e7a0f967653a3b98e85b0c2fa39be7d
Dan Lynn <git@danlynn.org>
2009-06-17 00:48:44 -0400

    Restricted get_prev_scan to scans of same dir

--- clamav.rb
+++ clamav.rb
@@ -123,7 +123,7 @@ end
 # gets the previous (chronologically by complete time) Scan instance to the
 # specified 'scan'.  Returns nil if no prev scan.
 def get_prev_scan(scan)
-  scan_ids = Scan.find(:all, :order => "complete", :select => "id")
+  scan_ids = Scan.find(:all, :conditions => ["dir = ?", scan.dir], :order => "complete", :select => "id")
   index = scan_ids.index(scan)
   return nil if index == 0
   Scan.find(scan_ids[index - 1])
